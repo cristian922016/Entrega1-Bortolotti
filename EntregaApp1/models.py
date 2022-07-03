e5a8114195f05079f28559994a4f83c791847700
unknown <crisadri872@gmail.com>
2022-07-03 16:31:52 -0300

    solo_falta_inputs

--- EntregaApp1/models.py
+++ EntregaApp1/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Block(models.Model):
+    titulo=models.CharField(max_length=30)
+    contenido=models.CharField(max_length=120)
+    fecha_creaccion=models.DateField(null=True)
--- EntregaApp1/views.py
+++ EntregaApp1/views.py
@@ -3,7 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 def una_vista(request):
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     return render(request,'index.html')
+
+
+
+
+
+
+
+
+
+
+
 
 
 def segunda_vista(request):
